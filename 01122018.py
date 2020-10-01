08c8a3f3a65aa42dd136cf96c5e34d2aad389c4c
nadimakram99 <42375102+nadimakram99@users.noreply.github.com>
2020-10-01 09:39:57 +0530

    Update 01122018.py

--- 01122018.py
+++ 01122018.py
@@ -6,14 +6,26 @@ class Stack:
         return self.items ==[]
 
     def push(self,item):
-        self.items.append(item)
+        self.items.append(item)  # adding the item in items 
 
     def pop(self):
-        return self.items.pop()
+        return self.items.pop()  #deleting the item on top
 
     def peek(self):
-        return self.items[len(self.items)-1]
+        return self.items[len(self.items)-1]   #returning tos (top of stack)
 
     def size(self):
-        return len(self.items)
-    
+        return len(self.items)    #returning the size of stack
+stack.push(5)
+stack.push(6)
+stack.push(7)
+
+stack.size()
+#output  :: 3
+#item is now [5,6,7]
+stack.is_empty()
+#output  :: [5,6,7]
+stack.pop()
+#output :: [5,6]
+stack.peek()
+#output :: 6
